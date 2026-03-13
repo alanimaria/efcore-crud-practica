b0131792d5b442a5152717a1de5a95c1f629af2b
Alani Encarnacion <alanimaria12@gmail.com>
2026-03-13 19:32:21 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoWebScrub.Models;
+
+namespace ProyectoWebScrub.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private static List<Cliente> clientes = new List<Cliente>()
+        {
+            new Cliente
+            {
+                Id = 1,
+                Nombre = "Juan Pérez",
+                Email = "juan@email.com",
+                Telefono = "8091234567",
+                FechaNacimiento = new DateOnly(1995,5,10)
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(clientes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Cliente cliente)
+        {
+            cliente.Id = clientes.Count + 1;
+
+            clientes.Add(cliente);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var cliente = clientes.FirstOrDefault(c => c.Id == id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+        [HttpPost]
+        public IActionResult Edit(Cliente cliente)
+        {
+            var clienteExistente = clientes.FirstOrDefault(c => c.Id == cliente.Id);
+
+            if (clienteExistente == null)
+            {
+                return NotFound();
+            }
+
+            clienteExistente.Nombre = cliente.Nombre;
+            clienteExistente.Email = cliente.Email;
+            clienteExistente.Telefono = cliente.Telefono;
+            clienteExistente.FechaNacimiento = cliente.FechaNacimiento;
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var cliente = clientes.FirstOrDefault(c => c.Id == id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            clientes.Remove(cliente);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿// TODO: Agrega [Required], [StringLength], [Display] apropiados
+// TODO: Agrega [Required], [EmailAddress], [Display]
+// TODO: Agrega [Phone] y que NO sea obligatorio
+// TODO: El cliente debe ser mayor de 18 años
+// Pista: usa [Range] con DateTime o implementa IValidatableObject
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoWebScrub.Models
+{
+    public class Cliente
+    {
+        public int Id { 
+            get; set; 
+        }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { 
+            get; set; 
+        } = "";
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo inválido")]
+        public string Email { 
+            get; set; 
+        } = "";
+
+        public string? Telefono { 
+            get; set; 
+        }
+
+        public DateOnly FechaNacimiento { 
+            get; set; }
+    }
+}
